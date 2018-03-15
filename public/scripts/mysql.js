7f92244a9a7ec5358e7a2f08fe5ba76d01566082
Henri Beck <henribeck.dev@gmail.com>
2018-03-15 22:18:12 +0100

    Moved HTML, CSS, JS and images into public folder

--- public/scripts/mysql.js
+++ public/scripts/mysql.js
@@ -1,5 +1,5 @@
 
-var mysql = require('mysql');
+var mysql = require('public/scripts/mysql');
  var con = mysql.createConnection({
     host: "192.168.179.60:3306",
     user: "root",
@@ -10,6 +10,6 @@ function check_connection_result(err){
 
     console.log('Connected');
 };
- 
+
 
  con.connect(check_connection_result);
