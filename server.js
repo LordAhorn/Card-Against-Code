8301ecaa4648be1ba438f5a848e758032cd04987
Henri Beck <henribeck.dev@gmail.com>
2018-03-15 22:18:50 +0100

    Fixed path and statically hosting the public/ folder

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ var app     = express();
 var path    = require("path");
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.resolve(__dirname, 'public//index.html'));
   //__dirname : The directory the file is in
 });
 
 app.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname, '/about.html'));
+  res.sendFile(path.resolve(__dirname, 'public/about.html'));
 });
 
 app.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname, '/sitemap.html'));
+  res.sendFile(path.resolve(__dirname, 'public//sitemap.html'));
 });
+
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.listen(3000, function () {
 	console.log("Running at Port 3000");
